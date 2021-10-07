1dc9d3b0087ffd78ef7dff78363584bfa5e11a79
Shawn Chen <shawn.chen@onedegree.hk>
2021-10-07 16:25:07 +0800

    define routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import {SWRConfig} from 'swr';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import 'src/styles/globals.css'
+
+const swrConfig = {
+  dedupingInterval: 2000,
+  revalidateOnFocus: false,
+}
+
+const reactQueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      staleTime: 2000,
+      cacheTime: 10000,
+      refetchOnWindowFocus: false,
+      notifyOnChangeProps: 'tracked',
+    }
+  }
+}
+
+const queryClient = new QueryClient(reactQueryClientConfig);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SWRConfig value={swrConfig}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
